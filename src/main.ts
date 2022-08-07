3a749f950fea7b4cbb23f29059714c182c3a5cd2
Dima <dikashko@gmail.com>
2022-08-07 16:22:00 +0300

    feat: implement custom ExceptionFilter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import parseYaml from './utils/parseYaml';
+import { AllExceptionsFilter } from './utils/all-exceptions.filter';
 
 const port = process.env.PORT;
 
@@ -10,6 +11,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger(AppModule.name, { timestamp: true });
   app.useGlobalPipes(new ValidationPipe());
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   const document = parseYaml();
   SwaggerModule.setup('api', app, <OpenAPIObject>document);
   await app.listen(port);
